308fa54a5161f917a5b8d717d42e3a52cd28c44e
Haarika Ramadugu <hramadugu@gmail.com>
2023-07-21 16:30:55 -0700

    fixed:fixed configuration for mobole

--- web/src/App.js
+++ web/src/App.js
@@ -8,42 +8,50 @@ import LoginSignUp from "./LoginSignup"
 import 'react-calendar/dist/Calendar.css';
 
 function App() {
-  const [user, setUser] = useState(null)
-  useEffect(() => {
-    fetch("/me")
-      .then((res) => res.json())
-      .then((data) => {
-        if (!data.errors) {
-          setUser(data)
-        }
+  // const [user, setUser] = useState(null)
+  // useEffect(() => {
+  //   fetch("/me")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       if (!data.errors) {
+  //         setUser(data)
+  //       }
 
-      })
-  }, [])
-  if (!user) return <LoginSignUp setUser={setUser} />
-  function handleLogout() {
-    console.log("Logging out")
-    fetch("/logout", {
-      method: "DELETE"
-    })
-      .then(resp => resp.json())
-      .then(setUser(null))
-  }
+  //     })
+  // }, [])
+  // if (!user) return <LoginSignUp setUser={setUser} />
+  // function handleLogout() {
+  //   console.log("Logging out")
+  //   fetch("/logout", {
+  //     method: "DELETE"
+  //   })
+  //     .then(resp => resp.json())
+  //     .then(setUser(null))
+  // }
 
   return (
     <>
       <div className="App-Container">
-        <div className='user'>
+        <header>
+          <button>Login</button>
+          <button>Create Account</button>
+        </header>
+        {/* <div className='user'>
           {user ? <button variant="contained" onClick={handleLogout}>Logout!</button> : null}
           <img src={user.image} alt="user" />
           <p>Hi,{user.username}!!</p>
-        </div>
+        </div> */}
 
-        <DestinationContainer user={user} setUser={setUser} />
-        <LoginSignUp setUser={setUser} />
+        {/* <DestinationContainer user={user} setUser={setUser} /> */}
+        {/* <LoginSignUp setUser={setUser} /> */}
         <br />
         <Route exact path="/useraccount">
-          <UserAccount user={user} />
+          {/* <UserAccount user={user} /> */}
         </Route>
+        <footer>
+          <link href='#'>About</link>
+          <link href='#'>Feedback</link>
+        </footer>
       </div>
     </>
   );
